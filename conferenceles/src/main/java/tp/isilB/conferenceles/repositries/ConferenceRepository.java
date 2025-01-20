4a59f8cfb0421829ff5f51861cdc06053d133c55
Silyaasadi <53907820+Silyaasadi@users.noreply.github.com>
2025-01-21 00:20:33 +0100

    Update ConferenceRepository.java

--- conferenceles/src/main/java/tp/isilB/conferenceles/repositries/ConferenceRepository.java
+++ conferenceles/src/main/java/tp/isilB/conferenceles/repositries/ConferenceRepository.java
@@ -2,7 +2,7 @@ package tp.isilB.conferenceles.repositries;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import tp.isilB.conferenceles.entities.Conference;
-
+import java.util.List;
 public interface ConferenceRepository extends JpaRepository<Conference, Long> {
  List<Conference> findByEditeurId(Long editeurId);
 
