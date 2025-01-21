64f081f7a0b1ef37577e131a1d3f8b72eaee9704
Silyaasadi <53907820+Silyaasadi@users.noreply.github.com>
2025-01-21 01:23:58 +0100

    Update ConferenceController.java

--- conferenceles/src/main/java/tp/isilB/conferenceles/controllers/ConferenceController.java
+++ conferenceles/src/main/java/tp/isilB/conferenceles/controllers/ConferenceController.java
@@ -3,6 +3,7 @@ package tp.isilB.conferenceles.controllers;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import tp.isilB.conferenceles.DTO.ConferenceDTO;
 import tp.isilB.conferenceles.entities.Conference;
 import tp.isilB.conferenceles.entities.Editeur;
 import tp.isilB.conferenceles.entities.Soumission;
@@ -15,57 +16,50 @@ import tp.isilB.conferenceles.services.SoumissionService;
 import java.util.List;
 import java.time.LocalDate;
 import java.util.List;
+
 @RestController
 @RequestMapping("/conferences")
 public class ConferenceController {
 
     @Autowired
-   private ConferenceService conferenceService;
-
-    @Autowired
-    private EditeurRepository editeurRepository;
-
-
-    @PostMapping("/{editeurId}")
-    public ResponseEntity<Conference> createConference(@PathVariable Long editeurId, @RequestBody Conference conference) {
-       Editeur editeur = editeurRepository.findById(editeurId)
-               .orElseThrow(() -> new RuntimeException("Éditeur introuvable avec l'ID : " + editeurId));
-
-       conference.setEditeur(editeur);
-        Conference createdConference = conferenceService.createConference(editeurId, conference);
-        return ResponseEntity.ok(createdConference);
-    }
-
-    @GetMapping
-    public List<Conference> getAllConferences() {
-      return conferenceService.getAllConferences();
-    }
-    @GetMapping("/editeur/{editeurId}")
-    public List<Conference> getConferencesByEditeur(@PathVariable Long editeurId) {
-        System.out.println("Requête pour les conférences de l'éditeur ID : " + editeurId);
-        return conferenceService.getConferencesByEditeur(editeurId);
-    }
-
-       @PutMapping("/{id}")
-        public ResponseEntity<Conference> updateConference(@PathVariable Long id, @RequestBody Conference conference) {
-            Conference updatedConference = conferenceService.updateConference(id, conference);
-            return ResponseEntity.ok(updatedConference);
-        }
-
-        @PatchMapping("/{id}/etat")
-        public ResponseEntity<Conference> updateConferenceState(@PathVariable Long id, @RequestBody String nouvelEtat) {
-            Conference updatedConference = conferenceService.updateConferenceState(id, nouvelEtat);
-            return ResponseEntity.ok(updatedConference);
-        }
-
-        @DeleteMapping("/{id}")
-        public ResponseEntity<Void> deleteConference(@PathVariable Long id) {
-             conferenceService.deleteConference(id);
-            return ResponseEntity.noContent().build();
-        }
+    private ConferenceService conferenceService;
 
     @Autowired
     private SoumissionService soumissionService;
+
+    @GetMapping
+    public ResponseEntity<List<ConferenceDTO>> getAllConferences() {
+        List<ConferenceDTO> conferences = conferenceService.getAllConferences();
+        return ResponseEntity.ok(conferences);
+    }
+
+    @PostMapping("/{editeurId}")
+    public ResponseEntity<ConferenceDTO> createConference(
+            @PathVariable Long editeurId,
+            @RequestBody ConferenceDTO conferenceDTO) {
+        ConferenceDTO createdConference = conferenceService.createConference(editeurId, conferenceDTO);
+        return ResponseEntity.ok(createdConference);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<ConferenceDTO> updateConference(
+            @PathVariable Long id,
+            @RequestBody ConferenceDTO conferenceDTO) {
+        ConferenceDTO updatedConference = conferenceService.updateConference(id, conferenceDTO);
+        return ResponseEntity.ok(updatedConference);
+    }
+
+    @PatchMapping("/{id}/etat")
+    public ResponseEntity<Conference> updateConferenceState(@PathVariable Long id, @RequestBody String nouvelEtat) {
+        Conference updatedConference = conferenceService.updateConferenceState(id, nouvelEtat);
+        return ResponseEntity.ok(updatedConference);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteConference(@PathVariable Long id) {
+        conferenceService.deleteConference(id);
+        return ResponseEntity.noContent().build();
+    }
 
     @PostMapping("/{conferenceId}/soumissions")
     public ResponseEntity<Soumission> addSoumissionToConference(
@@ -74,4 +68,4 @@ public class ConferenceController {
         Soumission savedSoumission = soumissionService.addSoumissionToConference(conferenceId, soumission);
         return ResponseEntity.ok(savedSoumission);
     }
-    }
+}
